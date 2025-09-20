066a23aa804c8b4e7a73a0ada48bbe2227625d34
Florian Rüberg <florian.rueberg@gmail.com>
2025-09-20 17:03:06 +0200

    Update files

--- setup/pc.ps1
+++ setup/pc.ps1
@@ -1,14 +1,14 @@
 # -- run in powershell using --
 # iex (irm "https://raw.githubusercontent.com/fru/infrastructure/main/setup/pc.ps1")
 
-$desktop = [System.Environment]::GetFolderPath('Desktop')
+$documents = [System.Environment]::GetFolderPath('mydocuments')
 $url = "https://raw.githubusercontent.com/fru/infrastructure/main/setup/pc.zip"
-$zip = "$desktop\pc.zip"
+$zip = "$documents\pc.zip"
 
 Invoke-WebRequest -Uri $url -OutFile $zip
-Expand-Archive -Path $zip -DestinationPath $desktop -Force
+Expand-Archive -Path $zip -DestinationPath $documents -Force
 Remove-Item $zip
 
-Start-Process powershell.exe -Verb RunAs -ArgumentList "-File $desktop\step-1-minimal.ps1" -Wait
-Start-Process powershell.exe -Verb RunAs -ArgumentList "-File $desktop\step-2-bloat.ps1" -Wait
-Start-Process powershell.exe -Verb RunAs -ArgumentList "-File $desktop\step-3-install.ps1" -Wait
+Start-Process powershell.exe -Verb RunAs -ArgumentList "-File $documents\setup-1-minimal.ps1" -Wait
+Start-Process powershell.exe -Verb RunAs -ArgumentList "-File $documents\setup-2-bloat.ps1" -Wait
+Start-Process powershell.exe -Verb RunAs -ArgumentList "-File $documents\setup-3-install.ps1" -Wait
