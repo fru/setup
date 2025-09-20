22381f14fcccbad5ed6d2162fc51e0ba30287ccb
Florian Rüberg <florian.rueberg@gmail.com>
2025-09-20 15:14:16 +0200

    Add install scripts

---
+++ setup/pc-zip-v1/step-1-minimal.ps1
@@ -0,0 +1,20 @@
+:: Needed to run .ps1 files (like npm)
+
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine -Force
+
+:: Update
+
+winget update winget
+
+:: Install
+
+winget install --id Zen-Team.Zen-Browser
+winget install --id M2Team.NanaZip
+winget install --id Chocolatey.Chocolatey
+winget install --id Bitwarden.Bitwarden
+winget install --id Google.GoogleDrive
+winget install --id Google.Chrome
+
+:: Show Instructions
+
+& "C:\Program Files\Zen Browser\zen.exe" "https://htmlpreview.github.io/?https://raw.githubusercontent.com/fru/infrastructure/main/setup/pc.html"
--- setup/pc.ps1
+++ setup/pc.ps1
@@ -1,4 +1,4 @@
-:: -- RUN in powershell using --
+:: -- run in powershell using --
 :: iex (irm "https://raw.githubusercontent.com/fru/infrastructure/main/setup/pc.ps1")
 
 $desktop = [System.Environment]::GetFolderPath('Desktop')
@@ -10,3 +10,5 @@ Expand-Archive -Path $zip -DestinationPath $desktop -Force
 Remove-Item $zip
 
 Start-Process powershell.exe -Verb RunAs -ArgumentList "-File $desktop\step-1-minimal.ps1"
+Start-Process powershell.exe -Verb RunAs -ArgumentList "-File $desktop\step-2-bloat.ps1"
+Start-Process powershell.exe -Verb RunAs -ArgumentList "-File $desktop\step-3-install.ps1"
