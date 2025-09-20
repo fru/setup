a0a7470b69e8210e20b795f9a16b8934babf0983
Florian Rüberg <florian.rueberg@gmail.com>
2025-09-20 12:51:52 +0200

    Update pc.bat

--- setup/pc.bat
+++ setup/pc.bat
@@ -1,3 +1,7 @@
 echo "Test"
 
 winget install --id=Zen-Team.Zen-Browser
+
+
+& "C:\Program Files\Zen Browser\zen.exe" "https://htmlpreview.github.io/?https://raw.githubusercontent.com/fru/infrastructure/main/setup/pc.html"
+
