7a3b5ab8659164f4d73d7199779b0f5e5a330f84
Florian Rüberg <florian.rueberg@gmail.com>
2025-09-20 18:57:25 +0200

    Update pc.ps1

--- setup/pc.ps1
+++ setup/pc.ps1
@@ -10,6 +10,7 @@ Invoke-WebRequest -Uri $url -OutFile $zip
 Expand-Archive -Path $zip -DestinationPath $documents -Force
 Remove-Item $zip
 
+Start-Process powershell.exe -Verb RunAs -ArgumentList "-Command Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine -Force" -Wait
 Start-Process powershell.exe -Verb RunAs -ArgumentList "-File $documents\setup-1-minimal.ps1" -Wait
 Start-Process powershell.exe -Verb RunAs -ArgumentList "-File $documents\setup-2-bloat.ps1" -Wait
 Start-Process powershell.exe -Verb RunAs -ArgumentList "-File $documents\setup-3-install.ps1" -Wait
